f61b9448ee4b5ab6bb1cff9d14e3910101bd7155
Frastio10 <frastio.agustiyan@gmail.com>
2021-12-01 09:56:27 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        green1: '#5aaa4e',
+        green2: '#27ae60',
+        blue1: '#00a5e1',
+        grey1: '#a0aec0',
+        yellow1: '#ceb546',
+        yellow2: '#be8228',
+        cream1: '#beab8d',
+        red1: '#a20e0e',
+        darkBlue1: 'rgb(44, 52, 124)',
+        dark1: '#1a202c'
+      },
+      fontFamily: {
+        'sans': ['Product Sans', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
